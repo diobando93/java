b9f109ce0d4d3e3abc6a749d4c90018b7462de4c
unknown <formacio@PC-1062663.formacio.local>
2021-11-04 13:10:12 +0100

    Ejercicios s0301

--- sprint1/Collections/src/ej3/Main.java
+++ sprint1/Collections/src/ej3/Main.java
@@ -1,5 +1,30 @@
 package ej3;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.TreeSet;
+
 public class Main {
+	
+	public static void main(String[] args) {
+        Generator gen = new Generator();
+
+        /////////////////////////////////
+        ArrayList<String> al = new ArrayList<String>();
+        for(int i=0; i<20;i++) {
+            al.add(gen.next());
+        }
+        ///////////////////////////////
+        HashSet<String> hs = new HashSet<String>();
+        for(int i=0; i<20;i++) {
+            hs.add(gen.next());
+        }
+        /////////////////////////////
+        TreeSet<String> ts = new TreeSet<String>();
+        for(int i=0; i<20;i++) {
+            ts.add(gen.next());
+        }
+
+    }
 
 }
