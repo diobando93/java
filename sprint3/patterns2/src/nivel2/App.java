e1032b925d851d9aab2a71cfdaeae1cb686c95f2
Israel Obando <devisrael93@gmail.com>
2022-01-18 11:15:19 +0100

    patterns 2 listo

--- sprint3/patterns2/src/nivel2/App.java
+++ sprint3/patterns2/src/nivel2/App.java
@@ -1,5 +1,14 @@
 package nivel2;
 
 public class App {
-
+	
+	public static void main(String [] args) {
+		Pago nike = new Debito();
+		Pago adidas = new paypal();
+		Zapatos rinconDeportivo = new Zapatos();
+		rinconDeportivo.pasarelaPago(nike);
+		rinconDeportivo.pasarelaPago(adidas);
+		
+	}
+	
 }
