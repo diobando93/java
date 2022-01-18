6c98716ef684d4e5c3d3a7ff0ec92468b66e54df
Israel Obando <devisrael93@gmail.com>
2022-01-18 11:12:01 +0100

    patterns 2 finalizado,

---
+++ sprint3/patterns3/src/nivel3/App.java
@@ -0,0 +1,12 @@
+package nivel3;
+
+public class App {
+	
+	public static void main(String[] args) {
+		//Stock stock = new Stock();
+		//stock.listProduct();
+		Floristeria f1 = new Floristeria();
+		f1.addStock("Arbol", "nogal","32.3", "14.5");
+	}
+
+}
