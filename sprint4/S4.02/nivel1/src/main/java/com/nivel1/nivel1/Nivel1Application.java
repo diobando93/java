cac9735411a4c9043c0d97b8411f234276832fa7
Israel Obando <devisrael93@gmail.com>
2022-02-08 12:16:19 +0100

    s402 nivel 3 con swagger terminado

--- sprint4/S4.02/nivel1/src/main/java/com/nivel1/nivel1/Nivel1Application.java
+++ sprint4/S4.02/nivel1/src/main/java/com/nivel1/nivel1/Nivel1Application.java
@@ -3,7 +3,10 @@ package com.nivel1.nivel1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+
 public class Nivel1Application {
 
 	public static void main(String[] args) {
